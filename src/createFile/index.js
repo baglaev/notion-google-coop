bd487e1dccc3564e02dea74881c816528eeda727
baglaev <bglvssh@yandex.ru>
2024-12-04 12:59:33 +0300

    update: createFile

--- src/createFile/index.js
+++ src/createFile/index.js
@@ -1,3 +1,28 @@
+// import { createEvents } from 'ics';
+// import { fetchNotionEvents } from '../utils/notionApi.js';
+
+// let cachedICS = '';
+
+// async function updateICSFile() {
+//   const events = await fetchNotionEvents();
+
+//   const { value, error } = createEvents(events);
+
+//   if (error) {
+//     console.error('Ошибка создания ICS файла:', error);
+//     return;
+//   }
+
+//   cachedICS = value;
+//   console.log('ICS файл обновлен:', new Date().toLocaleString());
+// }
+
+// function getCachedICS() {
+//   return cachedICS;
+// }
+
+// export { updateICSFile, getCachedICS };
+
 import { createEvents } from 'ics';
 import { fetchNotionEvents } from '../utils/notionApi.js';
 
@@ -6,14 +31,32 @@ let cachedICS = '';
 async function updateICSFile() {
   const events = await fetchNotionEvents();
 
-  const { value, error } = createEvents(events);
+  const updatedEvents = events.map((event) => ({
+    title: event.title,
+    start: event.start,
+    end: event.end,
+  }));
+
+  const { value, error } = createEvents(updatedEvents);
 
   if (error) {
     console.error('Ошибка создания ICS файла:', error);
     return;
   }
 
-  cachedICS = value;
+  const timeZoneSection = `
+BEGIN:VTIMEZONE
+TZID:Europe/Moscow
+BEGIN:STANDARD
+DTSTART:19700101T000000
+TZOFFSETFROM:+0300
+TZOFFSETTO:+0300
+TZNAME:MSK
+END:STANDARD
+END:VTIMEZONE
+  `.trim();
+
+  cachedICS = value.replace('BEGIN:VEVENT', `${timeZoneSection}\nBEGIN:VEVENT`);
   console.log('ICS файл обновлен:', new Date().toLocaleString());
 }
 
