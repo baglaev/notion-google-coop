de8200b2d42a682a4068771444b00a8cf00b0a26
baglaev <bglvssh@yandex.ru>
2024-12-08 12:57:47 +0300

    udpate: review

--- src/createFile/index.js
+++ src/createFile/index.js
@@ -1,28 +1,3 @@
-// import { createEvents } from 'ics';
-// import { fetchNotionEvents } from '../utils/notionApi.js';
-
-// let cachedICS = '';
-
-// async function updateICSFile() {
-//   const events = await fetchNotionEvents();
-
-//   const { value, error } = createEvents(events);
-
-//   if (error) {
-//     console.error('Ошибка создания ICS файла:', error);
-//     return;
-//   }
-
-//   cachedICS = value;
-//   console.log('ICS файл обновлен:', new Date().toLocaleString());
-// }
-
-// function getCachedICS() {
-//   return cachedICS;
-// }
-
-// export { updateICSFile, getCachedICS };
-
 import { createEvents } from 'ics';
 import { fetchNotionEvents } from '../utils/notionApi.js';
 
--- src/utils/notionApi.js
+++ src/utils/notionApi.js
@@ -1,6 +1,6 @@
 import { Client } from '@notionhq/client';
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 const databaseId = process.env.CALENDAR_DB;
